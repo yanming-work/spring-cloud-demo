079ca35d5b86892c831f35fce06ba19f6d13c255
yanming <yanming_work@163.com>
2019-02-28 10:53:39 +0800

    更新说明

--- eureka/eureka-provider/src/main/java/com/github/yanming/demo/spring/cloud/eureka/provider/EurekaProviderApplication.java
+++ eureka/eureka-provider/src/main/java/com/github/yanming/demo/spring/cloud/eureka/provider/EurekaProviderApplication.java
@@ -1,9 +1,12 @@
 package com.github.yanming.demo.spring.cloud.eureka.provider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @description :
@@ -19,9 +22,18 @@ public class EurekaProviderApplication {
         SpringApplication.run(EurekaProviderApplication.class, args);
     }
 
+    /**
+    自动注入request
+     **/
+    @Autowired
+    private HttpServletRequest request;
     @RequestMapping("/hello")
     public String hello(String name){
-        return "eureka-provider hello "+name;
+        String hostName=request.getServerName();
+        //获取服务器端口
+        int  hostPort= request.getServerPort();
+        String hello= "eureka-provider("+hostName+":"+hostPort+") say: hello "+name;
+        return hello;
     }
 
 }
